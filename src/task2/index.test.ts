353d7ab4b65d72f8e18b80617f1f01579ceba9ac
Daniel Norman <daniel.norman@flgtechnology.co.uk>
2022-07-13 20:46:12 +0100

    Rework with similar solution

--- src/task2/index.test.ts
+++ src/task2/index.test.ts
@@ -1,39 +1,7 @@
 import assert from 'assert';
-import findMeetingSlot, { Schedule, getSlotLength, findSlotsForSchedule, findEarliestSlot, Slot } from './';
+import findMeetingSlot, { Schedule } from './';
 
-describe('getSlotLength', function () {
-    it('should get the length of a slot', function () {
-        const slot : Slot = ['09:00', '11:30'];
-        const slotDuration = getSlotLength(slot);
-        console.log('slotDuration', slotDuration);
-        assert.deepEqual(slotDuration, 150);
-    });
-});
-
-describe('findSlotsForfindEarliestSlotSchedule', function () {
-    it('should find all the slots between meetings', function () {
-        const duration = 30;
-        const schedules : Schedule = [['09:00', '11:30'], ['13:30', '15:00'], ['16:00', '17:30'], ['17:45', '19:00']];
-        const slots = findSlotsForSchedule(schedules, duration);
-        assert.deepEqual(slots, [['11:30', '12:00'],['12:00', '12:30'],['12:30', '13:00'], ['13:00', '13:30'], ['15:00', '15:30'], ['15:30', '16:00']]);
-    });
-});
-
-describe.only('findEarliestSlot', function () {
-    it('should find the earliest slot that exists in all schedules', function () {
-        const timeslots: Schedule[] = [
-            [ [ '11:30', '12:00' ], [ '12:00', '12:30' ], [ '12:30', '13:00' ], [ '13:00', '13:30' ], [ '15:00', '15:30' ], [ '15:30', '16:00' ] ],
-            [ [ '12:00', '12:30' ], [ '12:30', '13:00' ], [ '13:00', '13:30' ], [ '13:30', '14:00' ], [ '14:30', '15:00' ], [ '15:00', '15:30' ], [ '15:30', '16:00' ], [ '16:00', '16:30' ], [ '16:30', '17:00' ], [ '17:30', '18:00' ], [ '18:00', '18:30' ], [ '18:30', '19:00' ] ],
-            // [ [ '09:00', '09:30' ], [ '09:30', '10:00' ], [ '10:00', '10:30' ], [ '10:30', '11:00' ], [ '11:00', '11:30' ], [ '12:15', '12:45' ], [ '12:45', '13:15' ], [ '13:15', '13:45' ], [ '13:45', '14:15' ], [ '14:15', '14:45' ], [ '14:45', '15:15' ], [ '15:15', '15:45' ] ],
-            [ [ '09:00', '09:30' ], [ '09:30', '10:00' ], [ '14:00', '14:30' ], [ '15:00', '15:30' ], [ '15:30', '16:00' ], [ '16:15', '16:45' ], [ '16:45', '17:15' ], [ '17:15', '17:45' ], [ '17:45', '18:15' ], [ '18:15', '18:45' ], [ '18:45', '19:15' ] ]
-        ];
-        const slots = findEarliestSlot(timeslots);
-        assert.deepEqual(slots, ['15:00', '15:30']);
-    });
-});
-
-
-describe('findAppointmentSlot', function () {
+describe.only('findAppointmentSlot', function () {
     it('should find the earliest appointment slot', function () {
         const schedules : Schedule[] = [
             [['09:00', '11:30'], ['13:30', '15:00'], ['16:00', '17:30'], ['17:45', '19:00']], // Schedule 1
@@ -44,6 +12,42 @@ describe('findAppointmentSlot', function () {
         const requiredDuration = 30;
         const slot = findMeetingSlot(schedules, requiredDuration);
         assert.deepEqual(slot, ['15:00', '15:30']);
+    });
+
+    it('should find the earliest appointment slot when it is at the beginning of the day', function () {
+        const schedules : Schedule[] = [
+            [['10:00', '11:30'], ['13:30', '15:00'], ['16:00', '17:30'], ['17:45', '19:00']], // Schedule 1
+            [['09:30', '12:00'], ['14:00', '14:30'], ['17:00', '17:30']], // Schedule 2
+            [['11:30', '12:15'], ['15:45', '19:00']], // Schedule 3
+            [['10:00', '14:00'], ['14:30', '15:00'], ['16:00', '16:15']], // Schedule 3
+        ];
+        const requiredDuration = 30;
+        const slot = findMeetingSlot(schedules, requiredDuration);
+        assert.deepEqual(slot, ['09:00', '09:30']);
+    });
+
+    it('should find the earliest appointment slot when it is at the end of the day', function () {
+        const schedules : Schedule[] = [
+            [['09:00', '11:30'], ['13:30', '15:00'], ['16:00', '17:30'], ['17:45', '18:00']], // Schedule 1
+            [['09:15', '12:00'], ['14:00', '14:30'], ['17:00', '17:30']], // Schedule 2
+            [['11:30', '12:15'], ['15:45', '18:30']], // Schedule 3
+            [['10:00', '14:00'], ['14:30', '16:00'], ['16:00', '16:15']], // Schedule 3
+        ];
+        const requiredDuration = 30;
+        const slot = findMeetingSlot(schedules, requiredDuration);
+        assert.deepEqual(slot, ['18:30', '19:00']);
+    });
+
+    it('should return null when there is no possible slot', function () {
+        const schedules : Schedule[] = [
+            [['09:00', '11:30'], ['13:30', '15:00'], ['16:00', '17:30'], ['17:45', '18:00']], // Schedule 1
+            [['09:15', '12:00'], ['14:00', '14:30'], ['17:00', '17:30']], // Schedule 2
+            [['11:30', '12:15'], ['15:45', '18:30']], // Schedule 3
+            [['10:00', '14:00'], ['14:30', '16:00'], ['16:00', '16:15']], // Schedule 3
+        ];
+        const requiredDuration = 90;
+        const slot = findMeetingSlot(schedules, requiredDuration);
+        assert.equal(slot, null);
     });
 });
 
--- src/task2/index.ts
+++ src/task2/index.ts
@@ -2,100 +2,37 @@
 export type Slot = [string, string];
 export type Schedule = Slot[];
 
-export function convertToMinutes(time: string) {
+function convertToMinutes(time: string) {
   const [hours, minutes] = time.split(':');
   return (Number(hours) * 60) + Number(minutes);
 }
 
-export function convertMinutesToString(timeInMinutes: number) {
+function convertMinutesToString(timeInMinutes: number) {
   const hours = Math.floor(timeInMinutes / 60);
   const minutes = timeInMinutes % 60;
   return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2,'0')}`;
 }
 
-export function getSlotLength(slot: Slot) {
-  return convertToMinutes(slot[1]) - convertToMinutes(slot[0]);
-}
+export function findEasliestCommonSlot(schedules: Schedule[], durationMinutes: number) {
+  let slotToCheckStart = convertToMinutes('09:00');
+  let slotToCheckEnd = slotToCheckStart + durationMinutes;
 
-export function splitSlotsIntoDurations(slots: Slot[], durationMinutes: number): Slot[] {
-  return slots.reduce((previous, slot) => {
-    if (getSlotLength(slot) === durationMinutes) previous.push(slot);
-    else {
-      const start = convertToMinutes(slot[0]);
-      const end = convertToMinutes(slot[1]);
+  while (slotToCheckEnd <= convertToMinutes('19:00')) {
+    const slotAvailable = schedules.every((schedule) => {
+      const conflictingMeeting = schedule.find(([start, end]) => {
+        return (slotToCheckStart >= convertToMinutes(start) && slotToCheckStart < convertToMinutes(end)) ||
+        (slotToCheckEnd > convertToMinutes(start) && slotToCheckEnd <=  convertToMinutes(end));
+      });
 
-      let currentStart = start;
-      let currentEnd = 0;
-
-      while (currentEnd < end) {
-        currentEnd = currentStart + durationMinutes;
-        console.log('durationMinutes', durationMinutes);
-        console.log('end', end);
-        console.log('currentEnd', currentEnd);
-
-        previous.push([convertMinutesToString(currentStart), convertMinutesToString(currentEnd)]);
-        currentStart = currentEnd;
-      }
-    }
-    return previous;
-  }, [] as Slot[]);
-}
-
-export function findSlotsForSchedule(schedule: Schedule, durationMinutes: number) : Slot[] {
-  const startOfWorkingDay = '09:00';
-  const endOfWorkingDay = '19:00';
-
-  const gaps: Schedule = [];
-
-  for (let i = 0; i < schedule.length; i++) {
-    const thisMeeting = schedule[i];
-    const nextMeeting = schedule[i+1];
-  
-    if (i === 0 && thisMeeting[0] != startOfWorkingDay) {
-      const firstGap: Slot = [startOfWorkingDay, thisMeeting[0]];
-      if (getSlotLength(firstGap) >= durationMinutes) gaps.push(firstGap);
-    }
-  
-    if (nextMeeting) {
-        const thisMeetingEnd = thisMeeting[1];
-        const nextMeetingStart = nextMeeting[0];
-        const newGap: Slot = [thisMeetingEnd, nextMeetingStart];
-        if (getSlotLength(newGap) >= durationMinutes) gaps.push(newGap);
-    }
-
-    if (i === schedule.length - 1 && thisMeeting[1] !== endOfWorkingDay) {
-      const lastGap: Slot = [thisMeeting[1], endOfWorkingDay];
-      if (getSlotLength(lastGap) >= durationMinutes) gaps.push(lastGap);
-    }
-  }
-
-  return splitSlotsIntoDurations(gaps, durationMinutes);
-}
-
-export function findEarliestSlot(suitableSlots: Schedule[]) {
-  const slotsInAllSchedules = suitableSlots[0].filter((slotToFind) => {
-    return suitableSlots.every(function(schedule) {
-        return schedule.findIndex((slot) => {
-          console.log('slot', slot);
-          console.log('slotToFind', slotToFind);
-          console.log('match?', slot[0] === slotToFind[0] && slot[1] === slotToFind[1]);
-          return slot[0] === slotToFind[0] && slot[1] === slotToFind[1];
-        }) !== -1;
+      return !conflictingMeeting;
     });
-  });
 
-  console.log('slotsInAllSchedules', slotsInAllSchedules);
+    if (slotAvailable) return [convertMinutesToString(slotToCheckStart), convertMinutesToString(slotToCheckEnd)];
 
-  slotsInAllSchedules.sort(([startA], [startB]) => {
-    return  convertToMinutes(startA) - convertToMinutes(startB);
-  });
-  return slotsInAllSchedules[0] ?? null; 
+    slotToCheckStart = slotToCheckStart + 5;
+    slotToCheckEnd = slotToCheckEnd + 5;
+  }
+  return null;
 }
 
-function findMeetingSlot(schedules: Schedule[], durationMinutes: number) {
-    const timeslots = schedules.map((schedule) => findSlotsForSchedule(schedule, durationMinutes));
-    console.log('time', timeslots);
-    return findEarliestSlot(timeslots);
-}
-
-export default findMeetingSlot;
+export default findEasliestCommonSlot;
